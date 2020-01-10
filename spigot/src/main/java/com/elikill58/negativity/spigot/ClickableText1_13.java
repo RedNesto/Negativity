850774f501eae4f6925797bdd02d661a306e0e29
RedNesto <RedNestoMC@gmail.com>
2020-01-10 16:42:30 +0100

    Remove unused imports that somehow made their way when setting up Gradle

--- spigot/src/main/java/com/elikill58/negativity/spigot/ClickableText1_13.java
+++ spigot/src/main/java/com/elikill58/negativity/spigot/ClickableText1_13.java
@@ -1,41 +1,8 @@
 package com.elikill58.negativity.spigot;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-import java.util.logging.Level;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import com.elikill58.negativity.spigot.ClickableText.Action;
 import com.elikill58.negativity.spigot.ClickableText.MessageComponent;
-import com.elikill58.negativity.universal.Cheat;
-import com.elikill58.negativity.universal.DefaultConfigValue;
-import com.elikill58.negativity.universal.NegativityAccount;
-import com.elikill58.negativity.universal.NegativityPlayer;
-import com.elikill58.negativity.universal.ReportType;
-import com.elikill58.negativity.universal.TranslatedMessages;
-import com.elikill58.negativity.universal.adapter.Adapter;
-import com.elikill58.negativity.universal.translation.CachingTranslationProvider;
-import com.elikill58.negativity.universal.translation.TranslationProvider;
-import com.elikill58.negativity.universal.translation.TranslationProviderFactory;
-import com.elikill58.negativity.universal.utils.UniversalUtils;
-import com.google.common.io.ByteStreams;
 
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
